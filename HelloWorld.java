4b3954694ace321864459ebeb165cbcbb093dced
Andrew Terebey <74176539+andrewterebey@users.noreply.github.com>
2025-04-02 15:12:34 -0700

    Create HelloWorld.java
    First Push

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        for (int num = 0; num <= 10; num++) {
+            System.out.println("Hello, World!");
+        }
+    }
+}

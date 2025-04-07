d9000d72d6fcf70b5e0cc23ba2033e8252aa9e00
Chie <145115707+Nadoshike@users.noreply.github.com>
2025-04-07 13:25:47 -0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         for (int num = 0; num <= 10; num++) {
             System.out.println("Hello, World!");
+            System.out.println("Test test");
         }
     }
 }
